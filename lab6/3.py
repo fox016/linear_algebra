378c426e38a84f5a51949a782f1067e3fe26018f
Nathan Fox <fox016@gmail.com>
2020-06-07 16:01:18 -0600

    working on 6-3

--- lab6/3.py
+++ lab6/3.py
@@ -5,15 +5,15 @@ xArr = []
 for x in range(5, 51, 5):
   xArr.push([1, x])
 x1 = np.array(xArr)
-print x1, y1
+print(x1, y1)
 
 xT = np.transpose(x1)
 normal_coef1 = np.matmul(xT, x1)
 normal_vect1 = np.matmul(xT, y1)
-print normal_coef1, normal_vect1
+print(normal_coef1, normal_vect1)
 
 beta1 = np.linalg.solve(normal_coef1, normal_vect1)
-print beta1
+print(beta1)
 
 beta2 = np.linalg.lstsq(x1, y1)
-print beta2
+print(beta2)
