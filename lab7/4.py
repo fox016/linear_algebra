5dbd8eec2ac963eb7d2b316c192563ed721ed52c
Nate Fox <nfox@byu.edu>
2020-06-24 15:00:45 -0600

    Through 7-3

--- lab7/4.py
+++ lab7/4.py
@@ -11,4 +11,22 @@ def plot1(x, y):
     plt.plot(x, y, 'o', markersize=1, color='red')
     plt.show()
 
-plot1(T, Y)
+def row_func(t, n):
+    L = [f(k) for k in [N*t for N in range(1,n+1)] for f in [np.cos, np.sin]]
+    L.insert(0, 1)
+    return L
+
+def design_matrix(n):
+    global T
+    L = []
+    for j in range(len(T)):
+        L.append(row_func(T[j], n))
+    matrix = np.array(L)
+    return matrix
+
+#plot1(T, Y)
+#print(row_func(2,5))
+#matrix = design_matrix(10)
+#print(np.shape(matrix))
+#matrix = design_matrix(4)
+#print(matrix[100,:])
