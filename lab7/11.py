459d7ff0b20849b7a950b65cb766c9fa16cd6830
Nate Fox <nfox@byu.edu>
2020-06-24 16:41:34 -0600

    lab 7

--- lab7/11.py
+++ lab7/11.py
@@ -24,9 +24,28 @@ def design_matrix(n):
     matrix = np.array(L)
     return matrix
 
+def curve_approx(n):
+    global T
+    global Y
+    x = design_matrix(n)
+    xT = np.transpose(x)
+    normal_coef = np.matmul(xT, x)
+    normal_vector = np.matmul(xT, Y)
+    beta = np.linalg.solve(normal_coef, normal_vector)
+    curve = np.array(list(map(lambda x: np.dot(beta, row_func(x, n)), T)))
+    plt.plot(T,Y,'r.')
+    plt.plot(T,curve,'b-')
+    plt.show()
+    mse = (1.0/629.0) * np.linalg.norm(np.subtract(np.matmul(x, beta), Y))**2
+    return mse
+
 #plot1(T, Y)
 #print(row_func(2,5))
 #matrix = design_matrix(10)
 #print(np.shape(matrix))
 #matrix = design_matrix(4)
 #print(matrix[100,:])
+print(curve_approx(2))
+print(curve_approx(10))
+print(curve_approx(100))
+print(curve_approx(1000))
